5c1045a01047c2b1f75c9c9b6d4d14b16a0b911d
Matheusvil <matheusvilela65@hotmail.com>
2019-08-09 19:03:11 -0300

    foi so pra testar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'cadastro', loadChildren: './cadastro/cadastro.module#CadastroPageModule' },
 ];
 
 @NgModule({
