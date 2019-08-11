ffce35296b8dc5457908012cce26325783bada2a
Matheusvil <matheusvilela65@hotmail.com>
2019-08-11 14:48:19 -0300

    Cadastro arrumado

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class LoginPage{
+  constructor(private navCtrl: NavController) {
   }
-
+  openBuscarPage() {
+    this.navCtrl.navigateForward('/buscar');
+  }
 }
