2ae9f926f91422c075f7d838d693338e0735e3a0
Willyan Antunes <willyan.dantunes@gmail.com>
2019-08-12 10:02:42 -0300

    pequenas alterações de layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, MenuController, NavController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+interface Page {
+  nome: string;
+  onClick(): void;
+}
 
 @Component({
   selector: 'app-root',
@@ -10,18 +15,44 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  pages: Array<Page> = [
+    {
+      nome: 'Perfil',
+      onClick: () => this.navCtrl.navigateForward('/perfil')
+    },
+    {
+      nome: 'Conversas',
+      onClick: () => {}
+    },
+    {
+      nome: 'Histórico',
+      onClick: () => {}
+    },
+    {
+      nome: 'Resultados',
+      onClick: () => {}
+    },
+    {
+      nome: 'Sair',
+      onClick: () => {}
+    }
+  ];
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private navCtrl: NavController
   ) {
     this.initializeApp();
   }
-
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  openPage(page: Page): void {
+    return;
   }
 }
--- src/app/buscar/buscar.page.ts
+++ src/app/buscar/buscar.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-buscar',
@@ -7,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private menu: MenuController) {
+  }
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
   ngOnInit() {
   }
 
