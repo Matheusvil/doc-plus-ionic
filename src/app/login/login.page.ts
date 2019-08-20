7b93e713fabb26cf5fa99437b428dad1d55bca5d
Matheusvil <matheusvilela65@hotmail.com>
2019-08-20 10:29:02 -0300

    autenticação do login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { FormGroup, FormBuilder, FormControl, Validators } from "@angular/forms"
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,27 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage{
-  constructor(private navCtrl: NavController) {
+
+  loginForm: FormGroup;
+
+  constructor(
+    private navCtrl: NavController, 
+    public formBuilder: FormBuilder
+  ) {
+    this.loginForm = this.formBuilder.group({
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(30),
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$') 
+      ])),
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(50),
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$') 
+      ]))
+    })
   }
   openBuscarPage() {
     this.navCtrl.navigateForward('/buscar');
