2a8940565e5157d12816c55867a9067ac9024654
Gabriel Matos <gsilva505@hotmail.com>
2019-08-21 15:31:39 -0300

    mapa implementado

--- src/app/buscar/buscar.page.ts
+++ src/app/buscar/buscar.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { Map, tileLayer, marker } from 'leaflet';
+
 
 @Component({
   selector: 'app-buscar',
@@ -15,6 +17,27 @@ export class BuscarPage implements OnInit {
     this.menu.open('first');
   }
   ngOnInit() {
+  }
+  map: Map;
+
+  ionViewDidEnter() { this.leafletMap(); }
+
+  leafletMap() {
+    // In setView add latLng and zoom
+    this.map = new Map('map').setView([-23.065814, -45.606919], 10);
+    tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'edupala.com © ionic LeafLet',
+    }).addTo(this.map);
+
+
+    marker([-23.065814, -45.606919]).addTo(this.map)
+      .bindPopup('Marker')
+      .openPopup();
+  }
+
+  /** Remove map when we have multiple map object */
+  ionViewWillLeave() {
+    this.map.remove();
   }
 
 }
